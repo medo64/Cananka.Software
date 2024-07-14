6351a976b7a874a17d2a363090a71a637b4ecc1d
Josip Medved <jmedved@jmedved.com>
2024-07-14 10:33:32 -0700

    Initial debug version

---
+++ src/CanankaDebug/App.cs
@@ -0,0 +1,64 @@
+﻿namespace CanankaDebug;
+using System;
+using System.IO.Ports;
+using System.Runtime.InteropServices.Marshalling;
+using System.Threading;
+using Medo.Device;
+
+internal static class CanankaDebug {
+
+    public static void Main(string[] args) {
+        if (args.Length != 1) {
+            Output.WriteErrorLine("Usage: canankadebug <serial-port>");
+            var portNames = SerialPort.GetPortNames();
+            if (portNames.Length > 0) {
+                Output.WriteErrorLine("Available ports:");
+                foreach (var portName in portNames) {
+                    Output.WriteErrorLine("  " + portName);
+                }
+            } else {
+                Output.WriteErrorLine("No ports available.");
+            }
+            Environment.Exit(255);
+        }
+
+        var serialPortName = args[0];
+
+        var cananka = new Cananka(serialPortName);
+        cananka.MessageArrived += OnMessageArrived;
+
+        try {
+            if (!cananka.Open()) {
+                Output.WriteErrorLine("Not a SLCAN device (" + serialPortName + ").");
+                Environment.Exit(1);
+            }
+        } catch (Exception ex) {
+            Output.WriteErrorLine("Error setting up device: " + ex.Message);
+            Environment.Exit(113);
+        }
+
+        Output.WriteOkLine("Connected to SLCAN device (" + serialPortName + ").");
+        Loop();
+
+        cananka.Close();
+        Output.WriteOkLine("Disconnected from SLCAN device (" + serialPortName + ").");
+    }
+
+
+    private static void Loop() {
+        while (true) {
+            var key = Console.ReadKey(intercept: true);
+            switch (key.Key) {
+                case ConsoleKey.Escape:
+                case ConsoleKey.Q:
+                    return;
+            }
+        }
+
+    }
+
+    private static void OnMessageArrived(object? sender, CanankaMessageEventArgs e) {
+        Output.WriteMessageLine(e.Message);
+    }
+
+}
---
+++ src/CanankaDebug/Output.cs
@@ -0,0 +1,66 @@
+namespace CanankaDebug;
+using System;
+using System.Globalization;
+using System.Reflection;
+using Medo.Device;
+
+internal static class Output {
+
+    private static readonly object Lock = new();
+
+    public static void WriteErrorLine(string text) {
+        lock (Lock) {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write(DateTime.Now.ToString("HH:mm:ss.fff", CultureInfo.InvariantCulture));
+            Console.Write(" ");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+
+    public static void WriteOkLine(string text) {
+        lock (Lock) {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write(DateTime.Now.ToString("HH:mm:ss.fff", CultureInfo.InvariantCulture));
+            Console.Write(" ");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+
+    public static void WriteMessageLine(CanankaMessage message) {
+        lock (Lock) {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write(DateTime.Now.ToString("HH:mm:ss.fff", CultureInfo.InvariantCulture));
+            Console.Write(" ");
+            Console.Write(message.ToString());
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            if (message.IsExtended) {
+                Console.Write(" " + message.Id.ToString("X8", CultureInfo.InvariantCulture));
+            } else {
+                Console.Write(" " + message.Id.ToString("X3", CultureInfo.InvariantCulture));
+            }
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(message.Length.ToString(" 0", CultureInfo.InvariantCulture));
+
+            if (message.Length > 0) {
+                Console.ForegroundColor = ConsoleColor.White;
+                var dataBytes = message.GetData() ?? [];
+                for (var i = 0; i < dataBytes.Length; i++) {
+                    Console.Write(" ");
+                    Console.Write(dataBytes[i].ToString("X2", CultureInfo.InvariantCulture));
+                }
+            }
+
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+    }
+
+}
